855668d75560f7d912dfff1e8647f61d86a465fd
Warren <wstix@ualberta.ca>
2020-11-02 12:08:13 -0700

    is the build passing

--- app/src/main/java/com/example/simpleparadox/listycity/City.java
+++ app/src/main/java/com/example/simpleparadox/listycity/City.java
@@ -17,6 +17,7 @@ public class City implements Comparable<City>{
         return this.province;
     }
 
+
     @Override
     public int compareTo(City o) {
         return city.compareTo(o.getCityName());
